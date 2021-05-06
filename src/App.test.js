a03b9ca60fbf57622aa4a28874cb754bd074d189
Jasmine Lane-Garcia <jasminerlanegarcia@gmail.com>
2021-05-06 09:52:18 -0800

    Added tests

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+
+// 'it()' similar to 'test' but can be used for previous
+// 'describe()'
+
+test("test 1", () => {
+    console.log("this is a test");
+});
+
+test("test 2", () => {
+    console.log("this is a test");
+});
+
+test("test 3", () => {
+    console.log("this is a test");
+});
